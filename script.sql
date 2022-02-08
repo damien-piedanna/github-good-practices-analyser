0a0cac2003b638d67fb3c1a46bc5d2410e87207a
tristan-laroubine <laroubine.tristan@yahoo.fr>
2022-02-08 21:13:40 +0100

    Update upper case and add script for Damien ❤️

--- script.sql
+++ script.sql
@@ -1,73 +1,86 @@
--- Count project number > 2000 lignes de codes
-select count(*)
-from projects
-where rowsOfCode > 2000;
+-- COUNT project number > 2000 lignes de codes
+SELECT COUNT(*)
+FROM projects
+WHERE rowsOfCode > 2000;
 
 
--- Repartisions of stars by project categories
-select category, count(p.stars) as total
-from categorizations c left join projects p on c.id = p.id
-where rowsOfCode > 2000
-group by category;
+-- RepartitiON of stars by project categories
+SELECT category, COUNT(p.stars) AS total
+FROM categorizatiONs c LEFT JOIN projects p ON c.id = p.id
+WHERE rowsOfCode > 2000
+GROUP BY category;
 
--- Somme des dependencies et moyenne des mauvaises dependencies
-select sum(dda.quantityOfDependencies + dda.quantityOfDevDependencies), AVG(dda.quantityOfWrongDevDependencies/dda.quantityOfTargetDependencies) as AVGOfWrongPlaceForDependancies
-from projects p
-    left join categorizations c on p.id = c.id
-    left join DevDependenciesAnalyzes dda on dda.id = c.id
-where
+-- Somme des Dependencies et moyennes des mauvaises Dependencies
+SELECT SUM(dda.quantityOfDependencies + dda.quantityOfDevDependencies), AVG(dda.quantityOfWrONgDevDependencies/dda.quantityOfTargetDependencies) AS AVGOfWrONgPlaceForDepENDancies
+FROM projects p
+    LEFT JOIN categorizatiONs c ON p.id = c.id
+    LEFT JOIN DevDependenciesAnalyzes dda ON dda.id = c.id
+WHERE
     c.category != 'other';
 
--- Query on % WrongDevDependenciesEvolutionByCategory
-select count(*)as total, c.category, AVG(dda.quantityOfWrongDevDependencies*100/dda.quantityOfTargetDependencies) as AVGOfWrongPlaceForDependancies
-from projects p
-    left join categorizations c on p.id = c.id
-    left join DevDependenciesAnalyzes dda on dda.id = c.id
-where
+-- Query ON % WrONgDevDependenciesEvolutiONByCategory
+SELECT FLOOR(p.contributors/10)*10 AS contributorsScale, c.category, AVG(dda.quantityOfWrONgDevDependencies*100/dda.quantityOfTargetDependencies) AS AVGOfWrONgPlaceForDepENDancies
+FROM projects p
+    LEFT JOIN categorizatiONs c ON p.id = c.id
+    LEFT JOIN DevDependenciesAnalyzes dda ON dda.id = c.id
+WHERE
     c.category != 'other'
-and dda.quantityOfTargetDependencies >= 1
-and p.rowsOfCode > 4000
-group by c.category;
+AND dda.quantityOfTargetDependencies >= 1
+AND p.rowsOfCode > 2000
+GROUP BY c.category,contributorsScale;
 
 
--- Query on the biggest project (contributor or rowof code) % WrongDevDependenciesEvolutionByCategory
-select p.name, p.contributors, p.rowsOfCode, c.category, dda.quantityOfWrongDevDependencies, dda.quantityOfTargetDependencies, (dda.quantityOfWrongDevDependencies*100/dda.quantityOfTargetDependencies)  as PourcentageOfWrongPlaceForDependancies
-from projects p
-    left join categorizations c on p.id = c.id
-    left join DevDependenciesAnalyzes dda on dda.id = c.id
-where
+
+-- Query ON the biggest project (cONtributor or rowof code) % WrONgDevDependenciesEvolutiONByCategory
+SELECT p.name, p.contributors, p.rowsOfCode, c.category, dda.quantityOfWrONgDevDependencies, dda.quantityOfTargetDependencies, (dda.quantityOfWrONgDevDependencies*100/dda.quantityOfTargetDependencies)  AS PourcentageOfWrONgPlaceForDepENDancies
+FROM projects p
+    LEFT JOIN categorizatiONs c ON p.id = c.id
+    LEFT JOIN DevDependenciesAnalyzes dda ON dda.id = c.id
+WHERE
     c.category != 'other'
--- order by p.contributors DESC;
-order by p.rowsOfCode DESC;
+-- ORDER BY p.contributors DESC;
+ORDER BY p.rowsOfCode DESC;
 
--- Export for Excel wrongUseOfDependancies
-select p.name, p.contributors, p.rowsOfCode, p.stars, c.category, (dda.quantityOfDependencies+dda.quantityOfDevDependencies) as depdenciesTotal, dda.quantityOfWrongDevDependencies, dda.quantityOfTargetDependencies , (dda.quantityOfWrongDevDependencies*100/dda.quantityOfTargetDependencies)  as PourcentageOfWrongPlaceForDependancies
-from projects p
-    left join categorizations c on p.id = c.id
-    left join DevDependenciesAnalyzes dda on dda.id = c.id
-where
+-- Export for Excel wrONgUseOfDepENDancies
+SELECT p.name, p.contributors, p.rowsOfCode, p.stars, c.category, (dda.quantityOfDependencies+dda.quantityOfDevDependencies) AS depdenciesTotal, dda.quantityOfWrONgDevDependencies, dda.quantityOfTargetDependencies , (dda.quantityOfWrONgDevDependencies*100/dda.quantityOfTargetDependencies)  AS PourcentageOfWrONgPlaceForDepENDancies
+FROM projects p
+    LEFT JOIN categorizatiONs c ON p.id = c.id
+    LEFT JOIN DevDependenciesAnalyzes dda ON dda.id = c.id
+WHERE
     c.category != 'other'
-and rowsOfCode > 2000
-order by p.contributors DESC;
+AND rowsOfCode > 2000
+ORDER BY p.contributors DESC;
 
 
--- Repartition des projets en fonction du nombre de contributeur
-select t.range as [Nb Contributeur], count(*) as [Nombre de projet]
-from (
-  select case
-    when projects.contributors between 0 and 9 then ' 0-9'
-    when projects.contributors between 10 and 19 then '10-19'
-    when projects.contributors between 20 and 29 then '20-29'
-    when projects.contributors between 30 and 39 then '30-39'
-    when projects.contributors between 40 and 49 then '40-49'
-    when projects.contributors between 50 and 59 then '50-59'
-    when projects.contributors between 60 and 69 then '60-69'
-    when projects.contributors between 70 and 79 then '70-79'
-    when projects.contributors between 80 and 89 then '80-89'
-    when projects.contributors between 90 and 99 then '90-99'
-    else '99+' end as range
-  from projects left join categorizations c on c.id = projects.id
-    where projects.rowsOfCode > 2000
-    and c.category != 'other' ) t
-group by t.range
+-- RepartitiON des projets en fONctiON du nombre de cONtributeur
+SELECT t.RANGE AS [Nb CONtributeur], COUNT(*) AS [Nombre de projet]
+FROM (
+  SELECT cASe
+    WHEN projects.contributors BETWEEN 0 AND 9 THEN ' 0-9'
+    WHEN projects.contributors BETWEEN 10 AND 19 THEN '10-19'
+    WHEN projects.contributors BETWEEN 20 AND 29 THEN '20-29'
+    WHEN projects.contributors BETWEEN 30 AND 39 THEN '30-39'
+    WHEN projects.contributors BETWEEN 40 AND 49 THEN '40-49'
+    WHEN projects.contributors BETWEEN 50 AND 59 THEN '50-59'
+    WHEN projects.contributors BETWEEN 60 AND 69 THEN '60-69'
+    WHEN projects.contributors BETWEEN 70 AND 79 THEN '70-79'
+    WHEN projects.contributors BETWEEN 80 AND 89 THEN '80-89'
+    WHEN projects.contributors BETWEEN 90 AND 99 THEN '90-99'
+    ELSE '99+' END AS RANGE
+  FROM projects LEFT JOIN categorizatiONs c ON c.id = projects.id
+    WHERE projects.rowsOfCode > 2000
+    AND c.category != 'other' ) t
+GROUP BY t.RANGE;
 
+-- EsLint usage % GROUP BY category AND contributorsScale
+
+SELECT FLOOR(p.contributors/10)*10 AS contributorsScale, c.category, COUNT(p.id) AS totalProject,
+       SUM(CASE WHEN ela.quantityOfPlugins > 1 OR ela.quantityOfRules > 2 THEN 1 ELSE 0 END) AS quantityOfEslint,
+       COUNT(p.id) / SUM(CASE WHEN ela.quantityOfPlugins > 1 OR ela.quantityOfRules > 2 THEN 1 ELSE 0 END) AS PourcentageOfEslintUsage
+FROM projects p
+    LEFT JOIN categorizatiONs c ON p.id = c.id
+    LEFT JOIN EsLintAnalyzes ela ON ela.id = c.id
+WHERE
+    c.category != 'other'
+AND p.rowsOfCode > 2000
+GROUP BY c.category,contributorsScale;
